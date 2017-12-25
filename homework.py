4e8d43358cd097b3b834df2c0f5833d0100336b2
Daniyar Zhanybaev <daniyar280028@gmail.com>
2017-12-25 21:29:56 +0600

    First commit

---
+++ homework.py
@@ -0,0 +1,20 @@
+# First 
+for i in range(1500, 2700):
+	if (i%7==0) and (i%5==0):
+		print (i)
+
+
+# Second
+z = 0
+while z<5:
+	print("*"*z)
+	z+=1
+	if z==5:
+		while z>0:
+			print("*"*z)
+			z-=1
+			if z==0:
+				break
+		break
+
+
